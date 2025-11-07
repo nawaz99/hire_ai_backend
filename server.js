b1f71715029134b65256c9f18e97ce32dba5ade0
Nawaz Sharief syed <nawaz@itradesoft.com>
2025-11-07 15:06:34 +0530

    updated vercel

--- server.js
+++ server.js
@@ -69,9 +69,9 @@ mongoose
 /* ========================
    ✅ Server
 ======================== */
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 
 export default app;
